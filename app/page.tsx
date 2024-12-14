7f8ee7a13f560c844511614c98776be62e3cee42
Piyush Patel <piyushpatelcodes@gmail.com>
2024-12-15 00:17:47 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Home
+      <Button variant="outline">Button</Button>
+
+    </div>
+  );
+}
