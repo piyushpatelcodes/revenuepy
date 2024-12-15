717fee4b04513e598f7b2c1eb9f5292b6e37df06
Piyush Patel <piyushpatelcodes@gmail.com>
2024-12-16 02:12:04 +0530

    added prettier plugin ref

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript","prettire"),
+  ...compat.extends("next/core-web-vitals", "next/typescript","plugin:prettier/recommended", "prettier"),
 ];
 
 export default eslintConfig;
